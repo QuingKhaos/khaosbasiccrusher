a7de889cd56ba7e81efe36a4949346b45b5dfb7d
Em Karisch <emii.khaos@gmail.com>
2026-08-04 10:04:36 +0200

    feat: use basic crusher graphics from AmatorPhasma (#1)

---
+++ graphics/entity/basic-crusher/basic-crusher-frozen.lua
@@ -0,0 +1,7 @@
+return
+{
+  width = 320,
+  height = 256,
+  shift = util.by_pixel(15, -8),
+  line_length = 1,
+}
--- prototypes/entity/basic-crusher-sa.lua
+++ prototypes/entity/basic-crusher-sa.lua
@@ -1,4 +1,4 @@
-require("__khaosbasiccrusher__.prototypes.entity.circuit-network")
+require("__khaosbasiccrusher__.prototypes.entity.circuit-network-sa")
 local hit_effects = require("__base__.prototypes.entity.hit-effects")
 local sounds = require("__base__.prototypes.entity.sounds")
 local khaoslib_entity = require("__khaoslib__.prototypes.entity")
@@ -9,8 +9,8 @@ local entity = khaoslib_entity:load {
   flags = {"placeable-neutral", "placeable-player", "player-creation"},
   fast_replaceable_group = "basic-crusher",
   max_health = 350,
-	corpse = "small-remnants",
-	dying_explosion = "medium-explosion",
+  corpse = "basic-crusher-remnants",
+  dying_explosion = "electric-furnace-explosion",
   circuit_wire_max_distance = 9,
   circuit_connector = circuit_connector_definitions["basic-crusher"],
   resistances = {
@@ -19,11 +19,14 @@ local entity = khaoslib_entity:load {
       percent = 80
     }
   },
-	collision_box = {{-1.2 * 0.4, -1.2 * 0.4}, {1.2 * 0.4, 1.2 * 0.4}},
-	selection_box = {{-1.5 * 0.4, -1.5 * 0.4}, {1.5 * 0.4, 1.5 * 0.4}},
+  collision_box = {{-0.7 * 0.5, -1.2 * 0.5}, {0.7 * 0.5, 1.2 * 0.5}},
+  selection_box = {{-1 * 0.5, -1.5 * 0.5}, {1 * 0.5, 1.5 * 0.5}},
   damaged_trigger_effect = hit_effects.entity(),
   module_slots = 0,
-  icon_draw_specification = {shift = util.by_pixel(0, -5), scale = 0.5},
+  icons_positioning = {
+    {inventory_index = defines.inventory.crafter_modules, shift = {0, 0.3}},
+  },
+  icon_draw_specification = {shift = {0.05, 0}, scale = 0.5},
   allowed_effects = {"consumption", "speed", "productivity", "pollution"},
   crafting_categories = {"basic-crushing"},
   crafting_speed = 2,
@@ -36,15 +39,30 @@ local entity = khaoslib_entity:load {
   open_sound = sounds.mech_small_open,
   close_sound = sounds.mech_small_close,
   working_sound = {
-		filename = "__base__/sound/burner-mining-drill-1.ogg",
-		volume = 1.0,
-		idle_sound = {filename = "__base__/sound/idle1.ogg", volume = 0.6},
+    sound = {
+      filename = "__khaosbasiccrusher__/sound/entity/basic-crusher-sa/basic-crusher-loop.ogg",
+      volume = 0.8,
+      audible_distance_modifier = 0.6,
+    },
+    fade_in_ticks = 4,
+    fade_out_ticks = 20,
+    max_sounds_per_prototype = 3,
   },
-  graphics_set = require("__khaosbasiccrusher__.prototypes.entity.basic-crusher-pictures"),
-} :set_icons {
-    {icon = "__khaosbasiccrusher__/graphics/icons/apm-machine-base-t2.png", icon_size = 64},
-    {icon = "__khaosbasiccrusher__/graphics/icons/apm-machine-label-c.png", icon_size = 64},
-  }
+  graphics_set = require("__khaosbasiccrusher__.prototypes.entity.basic-crusher-sa-pictures"),
+  water_reflection = {
+    pictures = {
+      filename = "__khaosbasiccrusher__/graphics/entity/basic-crusher-sa/basic-crusher-reflection.png",
+      priority = "extra-high",
+      width = 24,
+      height = 24,
+      shift = util.by_pixel(5, 40-32),
+      variation_count = 1,
+      scale = 5 * 0.5
+    },
+    rotate = false,
+    orientation_to_variation = false,
+  },
+} :set_icons {{icon = "__khaosbasiccrusher__/graphics/icons/basic-crusher-sa.png", icon_size = 64, scale = 0.8, tint = {0.7, 0.7, 0.7}}}
   :set_minable {mining_time = 0.2, result = "basic-crusher"}
 
 if mods["quality"] then
